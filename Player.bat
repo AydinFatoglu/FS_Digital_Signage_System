d7e8b13cf0e64999a1df023787e74b2cb95e835b
AydinFatoglu <86136084+AydinFatoglu@users.noreply.github.com>
2023-02-08 20:58:37 +0300

    Update Player.bat

--- Player.bat
+++ Player.bat
@@ -5,21 +5,54 @@ if %%a==ServerRootDir set ServerRootDir=%%b
 if %%a==LocalDir set LocalDir=%%b
 if %%a==ImageDuration set ImageDuration=%%b
 if %%a==Splash set Splash=%%b
-if %%a==PlayMode set PlayMode=%%b
 if %%a==ServerLayoutDirName set ServerLayoutDirName=%%b
 )
 
 SET CurrentDir="%~dp0"
 
+
+
 tasklist /nh /fi "imagename eq basewindow.exe" | find /i "basewindow.exe" > nul || (start "" %CurrentDir%basewindow.exe %Splash%)
+
+
+
+
+
+
+
 
 mkdir %LocalDir%
 
+ 
+
+
+
 ffmpeg -y -f lavfi -i anullsrc=r=44100:cl=mono -t %ImageDuration% -q:a 9 -acodec libmp3lame Duration.mp3
+
+
+
+
+
+
+
 
 %CurrentDir%Robocopy.exe %ServerRootDir%\%ServerLayoutDirName% %LocalDir% /xf *.db /MIR /FFT /Z /XA:H /R:3 /W:5
 
-%CurrentDir%mpv.exe --fullscreen  --audio-file=Duration.mp3 "%LocalDir%" %PlayMode%
+
+
+SET cnt=0
+for %%A in (%LocalDir%\*) do set /a cnt+=1
+if "%cnt%"=="1" (goto singlefile) else (goto multifile)
+
+
+
+
+:singlefile
+%CurrentDir%mpv.exe --fullscreen  --audio-file=Duration.mp3 "%LocalDir%" --loop-playlist
+
+
+:multifile
+%CurrentDir%mpv.exe --fullscreen  --audio-file=Duration.mp3 "%LocalDir%"
 
 
 SET "ServerRootDir="
@@ -28,5 +61,10 @@ SET "ImageDuration="
 SET "Splash="
 SET "PlayMode="
 SET "ServerLayoutDirName="
+SET "cnt="
+
+
+
+
 
 goto get
