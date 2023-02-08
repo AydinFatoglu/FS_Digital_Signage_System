2bfb0b74ce664c5c9f7c94596d22afeea2b017c8
AydinFatoglu <86136084+AydinFatoglu@users.noreply.github.com>
2023-02-08 20:17:41 +0300

    Update Player.bat

--- Player.bat
+++ Player.bat
@@ -1,34 +1,32 @@
-@echo off
-
-echo Starting up...
-timeout 5 >nul 2>&1 
-
-echo Player Running
-
-for /f "tokens=1,2,3,4 delims==" %%a in (Player.ini) do (
-if %%a==ServerDir set ServerDir=%%b
-if %%a==LocalDir set LocalDir=%%b
-if %%a==Duration set Duration=%%b
-if %%a==Splash set Splash=%%b
-)
-
-
-SET CurrentDir="%~dp0"
-SET ffmpegDir=%~dp0
-
-mkdir %LocalDir% >nul 2>&1
-
-start "" %CurrentDir%basewindow.exe %Splash% 
-
-%ffmpegtDir%ffmpeg -y -f lavfi -i anullsrc=r=44100:cl=mono -t %Duration% -q:a 9 -acodec libmp3lame Duration.mp3 >nul 2>&1
-
 :get
 
+for /f "tokens=1,2,3,4,5 delims==" %%a in (Player.ini) do (
+if %%a==ServerRootDir set ServerRootDir=%%b
+if %%a==LocalDir set LocalDir=%%b
+if %%a==ImageDuration set ImageDuration=%%b
+if %%a==Splash set Splash=%%b
+if %%a==PlayMode set PlayMode=%%b
+if %%a==ServerLayoutDirName set ServerLayoutDirName=%%b
+)
 
-TASKKILL /F /IM mpv.exe >nul 2>&1
+SET CurrentDir="%~dp0"
 
-%CurrentDir%Robocopyx %ServerDir% %LocalDir% /xf *.db /MIR /FFT /Z /XA:H /R:0 /W:2 >nul 2>&1
+tasklist /nh /fi "imagename eq basewindow.exe" | find /i "basewindow.exe" > nul || (start "" %CurrentDir%basewindow.exe %Splash%)
 
-%CurrentDir%mpv.exe --fullscreen  --audio-file=Duration.mp3 "%LocalDir%" >nul 2>&1
+mkdir %LocalDir%
+
+ffmpeg -y -f lavfi -i anullsrc=r=44100:cl=mono -t %ImageDuration% -q:a 9 -acodec libmp3lame Duration.mp3
+
+%CurrentDir%Robocopy.exe %ServerRootDir%\%ServerLayoutDirName% %LocalDir% /xf *.db /MIR /FFT /Z /XA:H /R:3 /W:5
+
+%CurrentDir%mpv.exe --fullscreen  --audio-file=Duration.mp3 "%LocalDir%" %PlayMode%
+
+
+SET "ServerRootDir="
+SET "LocalDir="
+SET "ImageDuration="
+SET "Splash="
+SET "PlayMode="
+SET "ServerLayoutDirName="
 
 goto get
