d570fa2dcec6579b58b1e91750afbe5c08b11d08
AydinFatoglu <86136084+AydinFatoglu@users.noreply.github.com>
2023-02-10 01:32:51 +0300

    Update Player.bat

--- Player.bat
+++ Player.bat
@@ -1,6 +1,22 @@
+clear
 :get
 
-for /f "tokens=1,2,3,4,5 delims==" %%a in (Player.ini) do (
+@echo off
+echo -----------------------------
+echo Checking Software Updates ...
+echo -----------------------------
+
+
+SET APPNAME=FS_Digtal_Signage
+
+
+
+xcopy %1\software\*.* %appdata%\%APPNAME%\ /I /D /S /Y > nul
+
+
+
+
+for /f "tokens=1,2,3,4,5 delims==" %%a in (%appdata%\%APPNAME%\%2.ini) do (
 if %%a==ServerRootDir set ServerRootDir=%%b
 if %%a==LocalDir set LocalDir=%%b
 if %%a==ImageDuration set ImageDuration=%%b
@@ -8,20 +24,23 @@ if %%a==Splash set Splash=%%b
 if %%a==ServerLayoutDirName set ServerLayoutDirName=%%b
 )
 
-SET CurrentDir="%~dp0"
-
-
-tasklist /nh /fi "imagename eq basewindow.exe" | find /i "basewindow.exe" > nul || (start "" %CurrentDir%basewindow.exe %Splash%)
-
-
-mkdir %LocalDir%
-
- 
-ffmpeg -y -f lavfi -i anullsrc=r=44100:cl=mono -t %ImageDuration% -q:a 9 -acodec libmp3lame Duration.mp3
 
 
 
-%CurrentDir%Robocopy.exe %ServerRootDir%\%ServerLayoutDirName% %LocalDir% /xf *.db /MIR /FFT /Z /XA:H /R:3 /W:5
+echo.
+echo -----------------------------
+echo Checking Basewindow ...
+echo -----------------------------
+cd %appdata%\%APPNAME%
+tasklist /nh /fi "imagename eq basewindow.exe" | find /i "basewindow.exe" > nul || (start "" basewindow.exe %Splash%)
+
+
+
+echo.
+echo -----------------------------
+echo Checking Content Changes ...
+echo -----------------------------
+Robocopy.exe %ServerRootDir%\%ServerLayoutDirName% %LocalDir% /xf *.db /MIR /FFT /Z /XA:H /R:3 /W:5 > nul
 
 
 
@@ -32,11 +51,21 @@ if "%cnt%"=="1" (goto singlefile) else (goto multifile)
 
 
 :singlefile
-%CurrentDir%mpv.exe --fullscreen  --audio-file=Duration.mp3 "%LocalDir%" --loop-playlist
+ffmpeg -y -f lavfi -i anullsrc=r=44100:cl=mono -t 900 -q:a 9 -acodec libmp3lame Duration.mp3 2>NUL
+echo.
+echo -----------------------------
+echo Playing Now ...
+echo -----------------------------
+mpv.exe --fullscreen  --audio-file=Duration.mp3 "%LocalDir%" > nul
 
 
 :multifile
-%CurrentDir%mpv.exe --fullscreen  --audio-file=Duration.mp3 "%LocalDir%"
+ffmpeg -y -f lavfi -i anullsrc=r=44100:cl=mono -t %ImageDuration% -q:a 9 -acodec libmp3lame Duration.mp3 2>NUL
+echo.
+echo -----------------------------
+echo Playing Now ...
+echo -----------------------------
+mpv.exe --fullscreen  --audio-file=Duration.mp3 "%LocalDir%" > nul
 
 
 SET "ServerRootDir="
@@ -47,6 +76,8 @@ SET "PlayMode="
 SET "ServerLayoutDirName="
 SET "cnt="
 SET "CurrentDir="
+SET "APPNAME="
+
 
 
 goto get
